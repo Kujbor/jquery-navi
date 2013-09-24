37a90a5fd14177489ddb73b43c0b3d686b503eb9
Oleg Taranov <Kujbor@ya.ru>
2013-09-24 13:58:35 +0400

    Adding navi.js

---
+++ navi.js
@@ -0,0 +1,124 @@
+/*
+ * navi.js - jQuery plugin to implement AJAX-navigation through HTML-attributes
+ * Author & copyright (C) 2013: Oleg Taranov aka Kujbor, CubeComp Development
+ */
+(function($) {
+
+    "use strict";
+
+    $.navi = new function() {
+
+        var self = this;
+
+        this.starters = "[ajax-target][ajax-action]";
+        this.domain = '';
+        this.cached = true;
+
+        this.actions = {};
+        this.requests = {};
+        this.cache = {};
+
+        this.init = function(options) {
+
+            if (options) {
+                self.setup(options);
+            }
+
+            $(self.starters).on("click", function() {
+                var elem = $(this);
+                self.navigate({
+                    target: elem.attr("ajax-target"),
+                    action: elem.attr("ajax-action"),
+                    cached: elem.attr("ajax-cached")
+                });
+            });
+
+        };
+
+        this.setup = function(options) {
+            $.extend($.ajaxNavigation, options);
+        };
+
+        this.navigate = function(options) {
+
+            var target = options.target;
+            var action = options.action;
+
+            if (self.actions[target] === action) {
+                return false;
+            }
+
+            self.actions[target] = action;
+
+            if (options.cached === "true") {
+                options.cached = true;
+            } else if (options.cached === "false") {
+                options.cached = false;
+            } else {
+                options.cached = self.cached;
+            }
+
+            if (options.cached) {
+
+                if (self.cache[target] && self.cache[target][action]) {
+                    self.show(options);
+                    return;
+                }
+
+            }
+
+            self.load(options);
+
+        };
+
+        this.show = function(options, template) {
+
+            var target = options.target;
+            var action = options.action;
+
+            if (!template) {
+                template = self.cache[target][action];
+            }
+
+            $(target).html(template);
+
+        };
+
+        this.load = function(options) {
+
+            var target = options.target;
+            var action = options.action;
+
+            if (self.requests[target]) {
+                self.requests[target].abort();
+            }
+
+            var url;
+
+            if (self.domain && action) {
+                url = self.domain + '/' + action;
+            } else {
+                url = self.domain + action;
+            }
+
+            self.requests[target] = $.get(url, function(template) {
+
+                if (options.cached) {
+
+                    if (!self.cache[target]) {
+                        self.cache[target] = {};
+                    }
+
+                    self.cache[target][action] = template;
+
+                }
+
+                self.show(options, template);
+
+            });
+
+        };
+
+    };
+
+})($);
